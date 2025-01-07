0f7b1b58609620ea2acaa5c9722e3f7a8bb164ed
VEN0M03 <pythonvenom291@gmail.com>
2025-01-07 16:36:54 +0100

    Delete JERX.py

--- JERX.py
+++
@@ -1,8 +0,0 @@
-import platform,os
-try:os.system('git pull')
-##try:os.system('xdg-open https://youtube.com/@Niki404-Cyber')
-except:pass
-try:
-    __import__("JERX").JERX()
-except Exception as e:
-    exit(str(e))
